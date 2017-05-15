f65e4a33a842844735d6b1369e21fd0ffb189e10
hornpa <hornpa@test.intra>
2017-05-15 12:34:45 +0200

    Upload new versions

---
+++ v1.08/PSM/hp_PSWrapper.psm1
@@ -0,0 +1,113 @@
+﻿####################################################################
+$PSM_Name = "PSWrapper - PowerShell Module"
+$PSM_Notes =@"
+Author:	Patrik Horn
+Link:	www.hornpa.de
+History:
+2016-08-12 - v1.01 - Add Get-IniFile, Out-IniFile (PHo)
+2016-05-10 - v1.00 - Release (PHo)
+"@
+####################################################################
+Write-Verbose "PSM Module: $PSM_Name "
+Write-Verbose "PSM Notes: $PSM_Notes "
+####################################################################
+Write-Verbose "Loading Functions..."
+# -------------------------------------------------------------------
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function Test-Transcribing {
+	$externalHost = $host.gettype().getproperty("ExternalHost",
+		[reflection.bindingflags]"NonPublic,Instance").getvalue($host, @())
+
+	try {
+	    $externalHost.gettype().getproperty("IsTranscribing",
+		[reflection.bindingflags]"NonPublic,Instance").getvalue($externalHost, @())
+	} catch {
+             write-warning "This host does not support transcription."
+         }
+}
+
+# Source http://www.remkoweijnen.nl/blog/2014/07/29/handling-ini-files-powershell/
+# PHo: Change regex Setting to read settings with '$' and '_'
+function Get-IniFile {
+    param (
+    	[parameter(mandatory=$true, position=0, valuefrompipelinebypropertyname=$true, valuefrompipeline=$true)][string]$FilePath
+    )
+
+	$ini = New-Object System.Collections.Specialized.OrderedDictionary
+	$currentSection = New-Object System.Collections.Specialized.OrderedDictionary
+	$curSectionName = "default"
+
+	switch -regex (gc $FilePath)
+	{
+	    "^\[(?<Section>.*)\]"
+	    {
+			$ini.Add($curSectionName, $currentSection)
+			
+			$curSectionName = $Matches['Section']
+			$currentSection = New-Object System.Collections.Specialized.OrderedDictionary	
+	    }
+		"(?<Key>.*)\=(?<Value>.*)"
+		{
+			# add to current section Hash Set
+			$currentSection.Add(($Matches['Key'] -replace "(['$''_'])"), $Matches['Value'])
+		}
+		"^$"
+		{
+			# ignore blank line
+		}
+		 
+		"(?<Key>\;)(?<Value>.*)"
+		{
+			$currentSection.Add($Matches['Key'], $Matches['Value'])	  
+		}
+			default
+		{
+			throw "Unidentified: $_"  # should not happen
+		}
+	}
+	if ($ini.Keys -notcontains $curSectionName) { $ini.Add($curSectionName, $currentSection) }
+	
+	return $ini
+}
+
+# Source http://www.remkoweijnen.nl/blog/2014/07/29/handling-ini-files-powershell/
+function Out-IniFile{
+    param (
+    	[parameter(mandatory=$true, position=0, valuefrompipelinebypropertyname=$true, valuefrompipeline=$true)][System.Collections.Specialized.OrderedDictionary]$ini,
+		[parameter(mandatory=$false,position=1, valuefrompipelinebypropertyname=$true, valuefrompipeline=$false)][String]$FilePath
+    )
+	
+	$output = ""
+	ForEach ($section in $ini.GetEnumerator())
+	{
+		if ($section.Name -ne "default") 
+		{ 
+			# insert a blank line after a section
+			$sep = @{$true="";$false="`r`n"}[[String]::IsNullOrWhiteSpace($output)]
+			$output += "$sep[$($section.Name)]`r`n" 
+		}
+		ForEach ($entry in $section.Value.GetEnumerator())
+		{
+			$sep = @{$true="";$false="="}[$entry.Name -eq ";"]
+			$output += "$($entry.Name)$sep$($entry.Value)`r`n"
+		}
+		
+	}
+	
+	$output = $output.TrimEnd("`r`n")
+	if ([String]::IsNullOrEmpty($FilePath))
+	{
+		return $output
+	}
+	else
+	{
+		$output | Out-File -FilePath $FilePath -Encoding:ASCII
+	}
+}
+
+# -------------------------------------------------------------------
+Write-Verbose "Finished"
---
+++ v1.09/Functions/03_Post/81_MS_RearmOS/Functions.ps1
@@ -0,0 +1,114 @@
+﻿#Requires -Version 3.0
+#Requires -RunAsAdministrator 
+#Requires -Modules hp_Log
+
+<#
+
+.SYNOPSIS
+    Rearm OS
+.Description
+    -
+.NOTES
+    Author: Patrik Horn (PHo)
+    Link:	www.hornpa.de
+    History:
+    2017-04-26 - Script created (PHo)
+
+#>
+
+Begin {
+#-----------------------------------------------------------[Pre-Initialisations]------------------------------------------------------------
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+#-----------------------------------------------------------[Main-Initialisations]------------------------------------------------------------
+
+	Write-Verbose "Function: Clear Error Variable Count"
+	$Error.Clear()
+	Write-Verbose "Function: Get PowerShell Start Date"
+	$StartPS_Sub = (Get-Date)
+	Write-Verbose "Set Variable with MyInvocation"
+	$scriptDirectory_Sub = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+	$scriptName_Sub = (Get-Help "$scriptDirectory_Sub\Functions.ps1").SYNOPSIS
+	Write-Verbose "Function Name: $scriptName_Sub"
+	Write-Verbose "Function Directory: $scriptDirectory_Sub"
+    Write-Host "Function: $($scriptName_Sub)" -ForegroundColor Green
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Process {
+	####################################################################
+	## Code Section - Start
+	####################################################################
+    
+    #region Check ReArm Count
+    $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform"
+    $RegName = "SkipRearm"
+    $RegValue = (Get-ItemProperty -Path $RegPath -Name $RegName).$RegName
+
+    IF ($RegValue -gt 0)
+    {
+    
+        $Msg = "The current ReArm count is $RegValue, will be reset to 0..."
+        Write-Log_hp -Path $LogPS -Message $Msg -Component $scriptName_Sub -Status Info
+        Try
+        {
+
+            $Msg = "successfully" 
+            Set-ItemProperty -Path $RegPath -Name $RegName -Value 1
+	        Write-Log_hp -Path $LogPS -Message $Msg -Component $scriptName_Sub -Status Info
+
+        }
+        Catch
+        {
+
+            $Msg = "error, could not reset Key to 0"
+            Write-Log_hp -Path $LogPS -Message $Msg -Component $scriptName_Sub -Status Error
+
+        }
+
+    }
+    Else
+    {
+        
+        $Msg = "The current ReArm count is $RegValue, everything is fine."
+        Write-Log_hp -Path $LogPS -Message $Msg -Component $scriptName_Sub -Status Info
+    
+    }
+    #endregion
+
+    #region ReArm OS
+    $cscript = "$env:windir\system32\cscript.exe" 
+    $Invoke = $cscript + " " + "slmgr.vbs /ReArm"
+
+    try 
+    { 
+        $Msg = "Succesfully, rearmed OS"
+	    Write-Log_hp -Path $LogPS -Message $Msg -Component $scriptName_Sub -Status Info
+        $Exe = Invoke-Expression $Invoke
+        $result = $true
+    }
+    catch 
+    {
+        $Msg = "Error, could not rearm OS."
+	    Write-Log_hp -Path $LogPS -Message $Msg -Component $scriptName_Sub -Status Error
+        $result = $false
+    }
+    #endregion
+	
+	####################################################################
+	## Code Section - End
+	####################################################################
+}
+
+#-----------------------------------------------------------[End]------------------------------------------------------------
+
+End {
+	Write-Verbose "Function: Get PowerShell Ende Date"
+	$EndPS_Sub = (Get-Date)
+	Write-Verbose "Function: Calculate Elapsed Time"
+	$ElapsedTimePS_Sub = (($EndPS_Sub-$StartPS_Sub).TotalSeconds)
+	$Msg = "Elapsed Time: $ElapsedTimePS_Sub Seconds"
+	Write-Log_hp -Path $LogPS -Message $Msg -Component $scriptName_Sub -Status Info
+}
